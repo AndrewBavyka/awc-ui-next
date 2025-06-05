87a57e90699a03e047ab45074a9f3034193fbc88
Andrew Bavyka <abavyka@list.ru>
2025-06-06 00:24:06 +0500

    chore: refactor component imports and organize code structure
    - Update .gitignore to include trailing slashes for directories
    - Reorganize imports in main.ts, grouping related components together
    - Move several components to a new 'form' folder:
      - AwcButton
      - AwcCheckbox
      - AwcCheckboxGroup
      - AwcColorPicker
      - AwcInput
      - AwcRadio
      - AwcRadioGroup
      - AwcRange
      - AwcSelect
      - AwcSelectGroup
      - AwcSelectItem
      - AwcSwitcher
      - AwcTextarea
    - Remove redundant components from export list

--- awc-ui/src/main.ts
+++ awc-ui/src/main.ts
@@ -1,29 +1,36 @@
+import AwcTab from './components/awc-tabs-group/awc-tab/awc-tab';
+import AwcTabsGroup from './components/awc-tabs-group/awc-tabs-group';
+import AwcAvatarGroupCounter from './components/awc-avatar/awc-avatar-group/awc-avatar-group-counter/awc-avatar-group-counter';
+import AwcCounter from './components/awc-counter/awc-counter';
+import AwcDivider from './components/awc-divider/awc-divider';
+import AwcSelectButton from './components/awc-select/awc-select-button/awc-select-button';
+import AwcDie from './components/awc-die/awc-die';
+import AwcRangeItem from './components/awc-range/awc-range-item/awc-range-item';
+import AwcEmptyState from './components/awc-empty-state/awc-empty-state';
+import AwcEmptyStateLink from './components/awc-empty-state/awc-empty-state-link/awc-empty-state-link';
+import AwcCard from './components/awc-card/awc-card';
+import AwcDialog from './components/awc-dialog/awc-dialog';
+
+
+
 import AwcAccordion from './components/awc-accordion/awc-accordion';
 import AwcAccordionItem from './components/awc-accordion/awc-accordion-item/awc-accordion-item';
 import AwcAlert from './components/awc-alert/awc-alert';
 import AwcAvatar from './components/awc-avatar/awc-avatar';
 import AwcAvatarBadge from './components/awc-avatar/awc-avatar-badge/awc-avatar-badge';
 import AwcAvatarGroup from './components/awc-avatar/awc-avatar-group/awc-avatar-group';
-import AwcAvatarGroupCounter from './components/awc-avatar/awc-avatar-group/awc-avatar-group-counter/awc-avatar-group-counter';
 import AwcBadge from './components/awc-badge/awc-badge';
-import AwcButton from './components/awc-button/awc-button';
+import AwcButton from './components/form/awc-button/awc-button';
 import AwcButtonGroup from './components/awc-button-group/awc-button-group';
 import AwcButtonGroupItem from './components/awc-button-group/awc-button-group-item/awc-button-group-item';
 import AwcCanControl from './components/development/awc-can-control/awc-can-control';
-import AwcCard from './components/awc-card/awc-card';
 import AwcChips from './components/awc-chips/awc-chips';
-import AwcCheckbox from './components/awc-checkbox-group/awc-checkbox/awc-checkbox';
-import AwcCheckboxGroup from './components/awc-checkbox-group/awc-checkbox-group';
-import AwcColorPicker from './components/awc-color-picker/awc-color-picker';
-import AwcCounter from './components/awc-counter/awc-counter';
-import AwcDialog from './components/awc-dialog/awc-dialog';
-import AwcDie from './components/awc-die/awc-die';
-import AwcDivider from './components/awc-divider/awc-divider';
+import AwcCheckbox from './components/form/awc-checkbox-group/awc-checkbox/awc-checkbox';
+import AwcCheckboxGroup from './components/form/awc-checkbox-group/awc-checkbox-group';
+import AwcColorPicker from './components/form/awc-color-picker/awc-color-picker';
 import AwcDropdown from './components/awc-dropdown/awc-dropdown';
 import AwcDropdownGroup from './components/awc-dropdown/awc-dropdown-group/awc-dropdown-group';
 import AwcDropdownItem from './components/awc-dropdown/awc-dropdown-item/awc-dropdown-item';
-import AwcEmptyState from './components/awc-empty-state/awc-empty-state';
-import AwcEmptyStateLink from './components/awc-empty-state/awc-empty-state-link/awc-empty-state-link';
 import AwcFile from './components/awc-file/awc-file';
 import AwcFileButton from './components/awc-file/awc-file-button/awc-file-button';
 import AwcFileItem from './components/awc-file/awc-file-item/awc-file-item';
@@ -31,20 +38,18 @@ import AwcHeader from './components/awc-header/awc-header';
 import AwcIcon from './components/awc-icon/awc-icon';
 import AwcIconButton from './components/awc-icon-button/awc-icon-button';
 import AwcIconLoader from './components/awc-icon-loader/awc-icon-loader';
-import AwcInput from './components/awc-input/awc-input';
+import AwcInput from './components/form/awc-input/awc-input';
 import AwcModal from './components/awc-modal/awc-modal';
 import AwcNotifier from './components/awc-notifier/awc-notifier';
 import AwcPagination from './components/awc-pagination/awc-pagination';
 import AwcPopover from './components/awc-popover/awc-popover';
 import AwcProgressBar from './components/awc-progress-bar/awc-progress-bar';
-import AwcRadio from './components/awc-radio-group/awc-radio/awc-radio';
-import AwcRadioGroup from './components/awc-radio-group/awc-radio-group';
-import AwcRange from './components/awc-range/awc-range';
-import AwcRangeItem from './components/awc-range/awc-range-item/awc-range-item';
-import AwcSelect from './components/awc-select/awc-select';
-import AwcSelectButton from './components/awc-select/awc-select-button/awc-select-button';
-import AwcSelectGroup from './components/awc-select/awc-select-group/awc-select-group';
-import AwcSelectItem from './components/awc-select/awc-select-item/awc-select-item';
+import AwcRadio from './components/form/awc-radio-group/awc-radio/awc-radio';
+import AwcRadioGroup from './components/form/awc-radio-group/awc-radio-group';
+import AwcRange from './components/form/awc-range/awc-range';
+import AwcSelect from './components/form/awc-select/awc-select';
+import AwcSelectGroup from './components/form/awc-select/awc-select-group/awc-select-group';
+import AwcSelectItem from './components/form/awc-select/awc-select-item/awc-select-item';
 import AwcSegmentSwitcher from './components/awc-segment-switcher/awc-segment-switcher';
 import AwcSegmentSwitcherItem from './components/awc-segment-switcher/awc-segment-switcher-item/awc-segment-switcher-item';
 import AwcSkeleton from './components/awc-skeleton/awc-skeleton';
@@ -53,17 +58,15 @@ import AwcSplitButton from './components/awc-split-button/awc-split-button';
 import AwcStack from './components/awc-stack/awc-stack';
 import AwcStager from './components/awc-stager/awc-stager';
 import AwcSteps from './components/awc-steps/awc-steps';
-import AwcSwitcher from './components/awc-switcher/awc-switcher';
+import AwcSwitcher from './components/form/awc-switcher/awc-switcher';
 import AwcTableWrapper from './components/awc-table-wrapper/awc-table-wrapper';
 import AwcTag from './components/awc-tag/awc-tag';
-import AwcTextarea from './components/awc-textarea/awc-textarea';
+import AwcTextarea from './components/form/awc-textarea/awc-textarea';
 import AwcToast from './components/awc-toast/awc-toast';
 import AwcToolbarButton from './components/awc-toolbar-group/awc-toolbar-button/awc-toolbar-button';
 import AwcToolbarGroup from './components/awc-toolbar-group/awc-toolbar-group';
 import AwcTooltip from './components/awc-tooltip/awc-tooltip';
 import AwcTripleToggler from './components/awc-triple-toggler/awc-triple-toggler';
-import AwcTabsGroup from './components/awc-tabs-group/awc-tabs-group';
-import AwcTab from './components/awc-tabs-group/awc-tab/awc-tab';
 import AwcUserInfo from './components/awc-user-info/awc-user-info';
 import AwcVueMounter from './components/development/awc-vue-mounter/awc-vue-mounter';
 
@@ -77,26 +80,18 @@ export default {
     AwcAvatar,
     AwcAvatarBadge,
     AwcAvatarGroup,
-    AwcAvatarGroupCounter,
     AwcBadge,
     AwcButton,
     AwcButtonGroup,
     AwcButtonGroupItem,
     AwcCanControl,
-    AwcCard,
     AwcChips,
     AwcCheckbox,
     AwcCheckboxGroup,
     AwcColorPicker,
-    AwcCounter,
-    AwcDialog,
-    AwcDie,
-    AwcDivider,
     AwcDropdown,
     AwcDropdownGroup,
     AwcDropdownItem,
-    AwcEmptyState,
-    AwcEmptyStateLink,
     AwcFile,
     AwcFileButton,
     AwcFileItem,
@@ -113,9 +108,7 @@ export default {
     AwcRadio,
     AwcRadioGroup,
     AwcRange,
-    AwcRangeItem,
     AwcSelect,
-    AwcSelectButton,
     AwcSelectGroup,
     AwcSelectItem,
     AwcSegmentSwitcher,
@@ -135,8 +128,6 @@ export default {
     AwcToolbarGroup,
     AwcTooltip,
     AwcTripleToggler,
-    AwcTabsGroup,
-    AwcTab,
     AwcUserInfo,
     AwcVueMounter,
 };
